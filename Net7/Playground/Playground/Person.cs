da9a6eddfe1ac8592795b85c7d1b496c1cd17f0e
Mateusz Kopij <sagasu@gmail.com>
2023-01-31 09:31:18 +0000

    checking some features

---
+++ Net7/Playground/Playground/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground;
+public class PersonOld
+{
+    public PersonOld(string fullName)
+    {
+        FullName = fullName;
+    }
+
+    public Guid ID { get; set; } = Guid.NewGuid();
+    public string FullName { get; set; }
+}
+
+
+public class Person
+{
+    public Guid ID { get; set; } = Guid.NewGuid();
+    public required string FullName { get; set; }
+}
+
---
+++ Net7/Playground/Playground/Program.cs
@@ -0,0 +1,8 @@
+﻿//namespace Playground;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+var p = new Playground.Person() { FullName = ""};
