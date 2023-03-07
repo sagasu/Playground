276a7188981e3f5a4cdaa894a8d5b8452c43c564
Mateusz Kopij <sagasu@gmail.com>
2023-03-07 08:17:59 +0000

    adding ef project

---
+++ Net7/EFWebProject/EFDataAccess/DataAccess/PeopleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFDataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFDataAccess.DataAccess
+{
+    public class PeopleContext : DbContext
+    {
+        public PeopleContext(DbContextOptions options) : base(options){}
+
+        public DbSet<Person> People { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Email> EmailAddresses { get; set; }
+    }
+}
