ac726de52f7b64d378a9f770edfa42a7c78d0dcb
Mateusz Kopij <sagasu@gmail.com>
2023-03-07 07:45:35 +0000

    adding UserData

---
+++ Net7/MinimalAPI/MinimalAPIWithDB.DataAccess/Data/UserData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MinimalAPIWithDB.DataAccess.DbAccess;
+using MinimalAPIWithDB.DataAccess.Models;
+
+namespace MinimalAPIWithDB.DataAccess.Data;
+
+public class UserData
+{
+    private readonly ISqlDataAccess _db;
+
+    public UserData(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+
+    public Task<IEnumerable<UserModel>> GetUsers() => _db.LoadData<UserModel, dynamic>("dbo.spUser_GetAll", new{});
+
+    public async Task<UserModel> GetUser(int id)
+    {
+        var results = await _db.LoadData<UserModel, dynamic>("dbo.spUser_Get", new { Id = id });
+        return results.FirstOrDefault();
+    }
+
+    public Task InsertUser(UserModel user) => _db.SaveData("dbo.spUser_Insert", new { user.FirstName, user.LastName });
+
+    public Task UpdateUser(UserModel user) => _db.SaveData("dbo.spUser_Update", user);
+
+    public Task DeleteUser(int id) => _db.SaveData("dbo.spUser_delete", new { Id = id });
+}
--- Net7/MinimalAPI/MinimalAPIWithDB.DataAccess/DbAccess/SqlDataAccess.cs
+++ Net7/MinimalAPI/MinimalAPIWithDB.DataAccess/DbAccess/SqlDataAccess.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace MinimalAPIWithDB.DataAccess.DbAccess;
 
-public class SqlDataAccess
+public interface ISqlDataAccess
 {
+    Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionId = "default");
+    Task SaveData<T>(string storedProcedure, T parameters, string connectionId = "default");
+}
+
+public class SqlDataAccess : ISqlDataAccess
+{
+    private readonly IConfiguration _config;
+
+    public SqlDataAccess(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public async Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionId = "default")
+    {
+        using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+
+        return await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+    }
+
+    public async Task SaveData<T>(string storedProcedure, T parameters, string connectionId = "default")
+    {
+        using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+
+        await connection.ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+    }
+
 
 }
